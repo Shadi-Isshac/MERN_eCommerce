4347c2572e1badab111b4f969237aa7875922183
Shadi Isshac <shadi@Shadis-MacBook-Pro.local>
2022-08-19 17:58:17 -0500

    fixed homescreen productscreen and cartscreen routes to display. began navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 
 
+
 function App() {
   return (
-    <Router>
-    <main>
+  
+  <Router>
+   
       <Routes>
-        <Route exact path="/" component={HomeScreen}/>
-        <Route exact path="/product/:id" component={ProductScreen}/>
-        <Route exact path="/cart" component={CartScreen}/>
+        <Route exact path="/" element={<HomeScreen/>}/>
+        <Route exact path="/product/:id" element={<ProductScreen/>}/>
+        <Route exact path="/cart" element={<CartScreen/>}/>
       </Routes>
-    </main>
+      
     </Router>
   );
 }
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -12,10 +12,24 @@ const navbar = () => {
       <ul className="navbar__links">
         <li>
             <Link to="/cart">
-
+              <i className=""></i> 
+              cart
+            <span className="cartlogo__badge">0</span>
+            </Link>
+        </li>
+        <li>
+            <Link to="/">
+              Shop
+            <span className="cartlogo__badge">0</span>
             </Link>
         </li>
       </ul>
+
+    <div className="hamburger__menu">
+    <div></div>
+    <div></div>
+    <div></div>
+    </div>
     </nav>
   )
 }
