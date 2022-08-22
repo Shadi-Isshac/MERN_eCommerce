2ae1124b53987b9a76be89614e6919c797c7fe10
Shadi Isshac <shadi@Shadis-MacBook-Pro.local>
2022-08-22 13:20:05 -0500

    product screen for individual prodects formatted

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -2,8 +2,37 @@ import './ProductScreen.css';
 
 const ProductScreen = () => {
   return (
-    <div className="productsScreen"> 
-    
+    <div className="productscreen"> 
+      <div className="productscreen__left">
+        <div className="left__image">
+          <img src="https://bjs.scene7.com/is/image/bjs/23459?$bjs-Zoom$" alt="Product name" />
+        </div>
+        <div className="left__info">
+          <p className="left__name">Product 1</p>
+          <p className="left__name">Price: </p>
+          <p className="left__name">Description: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem laboriosam voluptate expedita explicabo illo molestias?</p>
+        </div>
+      </div>
+      <div className="productscreen__right">
+        <div className="right__info">
+          <p>
+            Price: <span>$14.99</span>
+          </p>
+          <p>
+            Status: <span>In stock</span>
+          </p>
+          <p>
+            Qty
+            <select>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+            </select>
+            <button type='button'>Add To Cart</button>
+          </p>
+      </div>
+    </div>
     </div>
   )
 }
