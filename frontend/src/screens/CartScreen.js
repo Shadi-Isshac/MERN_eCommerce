d3d5ed08b0631c71810877c6eb628f842576222d
Shadi Isshac <shadi@Shadis-MacBook-Pro.local>
2022-08-22 13:56:28 -0500

    cart item and screen designed and mobile friendly

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,9 +1,25 @@
 import './CartScreen.css';
+import CartItem from '../components/CartItem';
 
 const CartScreen = () => {
   return (
     <div className="cartscreen">
-        Cart screen
+        <div className="cartscreen__left">
+          <h2>Shopping Cart</h2>
+          <CartItem />
+          <CartItem />
+          <CartItem />
+          <CartItem />
+        </div>
+       < div className="cartscreen__right"> 
+        <div className="cartscreen__info">
+          <p>Subtotal (0) items</p>
+          <p>$14.99</p>
+        </div>
+        <div>
+          <button>Proceed To Checkout</button>
+        </div>
+        </div>
     </div>
   )
 }
