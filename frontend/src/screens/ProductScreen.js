f4484fb77e786529b93527524a7dd3aab43c2213
Shadi Isshac <shadi@Shadis-MacBook-Pro.local>
2022-08-29 17:44:25 -0500

    cart items display dynamically and able to register change of quantity in Redux

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -2,46 +2,83 @@ import "./ProductScreen.css";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getProductDetails } from "../redux/actions/productActions";
+import { addToCart } from "../redux/actions/cartActions";
+import { useParams, useNavigate } from "react-router-dom";
 
 
 
-const ProductScreen = () => {
- 
+const ProductScreen = ({ match, history }) => {
+  const [qty, setQty] = useState(1);
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const navigate = useNavigate();
+  
 
- 
+  const productDetails = useSelector((state) => state.getProductDetails);
+  const { loading, error, product } = productDetails;
+
+  useEffect(() => {
+    if (product && id !== product._id) {
+      dispatch(getProductDetails(id));
+    }
+  }, [dispatch, product, match]);
+
+  const addToCartHandler = () => {
+    dispatch(addToCart(product._id, qty));
+    navigate(`/cart`);
+  };
+
   return (
-    <div className="productscreen"> 
-      <div className="productscreen__left">
-        <div className="left__image">
-          <img src="https://bjs.scene7.com/is/image/bjs/23459?$bjs-Zoom$" alt="Product name" />
-        </div>
-        <div className="left__info">
-          <p className="left__name">Cat Food</p>
-          <p className="left__name">Price: </p>
-          <p className="left__name">Description: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem laboriosam voluptate expedita explicabo illo molestias?</p>
-        </div>
-      </div>
-      <div className="productscreen__right">
-        <div className="right__info"></div>
-          <p>
-            Price: <span>$14.99</span>
-          </p>
-          <p>
-            Status: <span>In stock</span>
-          </p>
-          <p>
-            Qty
-            <select>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-            </select>
-            <button type='button'>Add To</button>
-          </p>
-      </div>
+    <div className="productscreen">
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h2>{error}</h2>
+      ) : (
+        <>
+          <div className="productscreen__left">
+            <div className="left__image">
+              <img src={product.imageUrl} alt={product.name} />
+            </div>
+            <div className="left__info">
+              <p className="left__name">{product.name}</p>
+              <p>Price: ${product.price}</p>
+              <p>Description: {product.description}</p>
+            </div>
+          </div>
+          <div className="productscreen__right">
+            <div className="right__info">
+              <p>
+                Price:
+                <span>${product.price}</span>
+              </p>
+              <p>
+                Status:
+                <span>
+                  {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                </span>
+              </p>
+              <p>
+                Qty
+                <select value={qty} onChange={(e) => setQty(e.target.value)}>
+                  {[...Array(product.countInStock).keys()].map((x) => (
+                    <option key={x + 1} value={x + 1}>
+                      {x + 1}
+                    </option>
+                  ))}
+                </select>
+              </p>
+              <p>
+                <button type="button" onClick={addToCartHandler}>
+                  Add To Cart
+                </button>
+              </p>
+            </div>
+          </div>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ProductScreen
+export default ProductScreen;
